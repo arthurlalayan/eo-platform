a92e3288328a45d6fdb7a2f6b832f5522035a96e
Arthur Lalayan <arthur.lalayan@synisys.com>
2022-11-09 12:29:04 +0400

    fixes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import json
 import time
 
+import dask
+import numpy as np
 import stackstac
-from distributed import performance_report
+from distributed import Client
 from fastapi import FastAPI
 from satsearch import Search
 
@@ -107,14 +109,49 @@ async def mem(data: RequestDataDto):
                         url=data.dataRepositoryURL)
         items = search.items()
 
-        stack = stackstac.stack(items, epsg=data.epsg, chunksize=data.chunk_size, bounds=bbox)
+        stack = stackstac.stack(items, epsg=data.epsg, chunksize=data.chunk_size, bounds=bbox, assets=["B08", "B04"])
         nir, red = stack.sel(band="B08"), stack.sel(band="B04")
-        with performance_report(filename="dask-report.html"):
-            start_time = time.time()
+        start_time = time.time()
+        try:
             ndvi = ((nir - red) / (nir + red))
             ndvi = ndvi.compute()
-            end_time = time.time()
-            return {'exec_time': end_time - start_time}
+        except Exception as e:
+            print(e)
+        end_time = time.time()
+        return {'exec_time': end_time - start_time}
+    except Exception as e:
+        print(e)
+    finally:
+        cluster.shutdown()
+
+
+@app.post("/ndvi_season")
+async def mem(data: RequestDataDto):
+    cluster = create_cluster(data.computationURL, data.nodes)
+    client = Client()
+    print(cluster.dashboard_link)
+    try:
+        bbox = get_bbox(data.geometry)
+        executor = Executor(data.dataRepositoryURL)
+        search = Search(bbox=bbox, datetime=data.date_range,
+                        query={"eo:cloud_cover": {"lt": data.scene_cloud_tolerance}},
+                        collections=["sentinel-s2-l2a-cogs"],
+                        url=data.dataRepositoryURL)
+        items = search.items()
+
+        stack = stackstac.stack(items, epsg=data.epsg, chunksize=data.chunk_size, bounds=bbox, assets=["B08", "B04"])
+        nir, red = stack.sel(band="B08"), stack.sel(band="B04")
+        start_time = time.time()
+        try:
+            ndvi = ((nir - red) / (nir + red))
+            ndvi = ndvi.mean("time").compute()
+            print(ndvi)
+            ndvi = ndvi.mean()
+            print(ndvi)
+        except Exception as e:
+            print(e)
+        end_time = time.time()
+        return {'exec_time': end_time - start_time}
     except Exception as e:
         print(e)
     finally:
@@ -158,6 +195,129 @@ async def ndvi(data: RequestDataDto):
             json.dump(meta, json_file, ensure_ascii=False)
             filenames.append('meta.json')
         return zip_files(filenames)
+    except Exception as e:
+        print(e)
+    finally:
+        cluster.shutdown()
+
+
+@app.post("/ndvi_1")
+async def ndvi_1(data: RequestDataDto):
+    cluster = create_cluster(data.computationURL, data.nodes)
+    client = cluster.get_client()
+    print(cluster.dashboard_link)
+    try:
+        bbox = get_bbox(data.geometry)
+        search = Search(bbox=bbox, datetime=data.date_range,
+                        query={"eo:cloud_cover": {"lt": data.scene_cloud_tolerance}},
+                        collections=["sentinel-s2-l2a-cogs"],
+                        url=data.dataRepositoryURL)
+        items = search.items()
+        results = []
+        for i, item in enumerate(items):
+            start_time = time.time()
+            stack = stackstac.stack(item, epsg=data.epsg, chunksize=data.chunk_size)
+            nir, red = stack.sel(band="B08"), stack.sel(band="B04")
+            ndvi_result = ((nir - red) / (nir + red)).compute()
+            end_time = time.time()
+            results.append(
+                {'index': i, 'size': (np.prod(nir.shape) / 1024 / 1024 * 8 * 2), 'exec_time': end_time - start_time})
+        return results
+    except Exception as e:
+        print(e)
+    finally:
+        cluster.shutdown()
+
+
+@app.post("/ndvi_submit")
+async def ndvi_submit(data: RequestDataDto):
+    cluster = create_cluster(data.computationURL, data.nodes)
+    client = Client()
+    print(cluster.dashboard_link)
+    try:
+        bbox = get_bbox(data.geometry)
+        search = Search(bbox=bbox, datetime=data.date_range,
+                        query={"eo:cloud_cover": {"lt": data.scene_cloud_tolerance}},
+                        collections=["sentinel-s2-l2a-cogs"],
+                        url=data.dataRepositoryURL)
+        items = search.items()
+        start_time = time.time()
+
+        stack = stackstac.stack(items, epsg=data.epsg, chunksize=data.chunk_size, assets=["B08", "B04"])
+        size = (np.prod(stack.shape) / 1024 / 1024 * 8)
+
+        def calculate(item):
+            stack = stackstac.stack(item, epsg=4326, chunksize=(1024, 1024), assets=["B08", "B04"])
+            nir, red = stack.sel(band="B08"), stack.sel(band="B04")
+            return (nir - red) / (nir + red)
+
+        futures = client.map(calculate, items)
+        results = client.gather(futures)
+        print(results[0])
+
+        end_time = time.time()
+        return {'size': size, 'exec_time': end_time - start_time}
+    except Exception as e:
+        print(e)
+    finally:
+        cluster.shutdown()
+
+
+@app.post("/testt")
+async def testt(data: RequestDataDto):
+    cluster = create_cluster(data.computationURL, data.nodes)
+    client = Client()
+    print(cluster.dashboard_link)
+    try:
+        start_time = time.time()
+
+        # @dask.delayed
+        def calculate(item):
+            return item + 1
+
+        futures = client.map(calculate, range(100))
+        results = client.gather(futures)
+        print(results[0])
+
+        # futures = client.map(calculate, L)
+        # results = await client.gather(futures, asynchronous=True)
+        # print(results)
+
+        # r = dask.compute(L)
+        # futures = client.compute(L)
+
+        end_time = time.time()
+        return {'exec_time': end_time - start_time}
+
+    except Exception as e:
+        print(e)
+    finally:
+        cluster.shutdown()
+
+
+@app.post("/ndvi_same/{count}")
+async def ndvi_same(data: RequestDataDto, count: int):
+    cluster = create_cluster(data.computationURL, data.nodes)
+    client = cluster.get_client()
+    print(cluster.dashboard_link)
+    try:
+        bbox = get_bbox(data.geometry)
+        search = Search(bbox=bbox, datetime=data.date_range,
+                        query={"eo:cloud_cover": {"lt": data.scene_cloud_tolerance}},
+                        collections=["sentinel-s2-l2a-cogs"],
+                        url=data.dataRepositoryURL)
+        items = search.items()
+        results = []
+        item = items[0]
+        for i in range(count):
+            start_time = time.time()
+            stack = stackstac.stack(item, epsg=data.epsg, chunksize=data.chunk_size)
+            nir, red = stack.sel(band="B08"), stack.sel(band="B04")
+            ndvi_result = ((nir - red) / (nir + red)).compute()
+            end_time = time.time()
+            results.append(
+                {'index': i, 'size': (np.prod(nir.shape) / 1024 / 1024 * 8 * 2), 'exec_time': end_time - start_time})
+        return results
     except Exception as e:
         print(e)
     finally:
