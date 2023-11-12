a57e21e698ea262c597191feca15e93f76289533
arturl <arturl@energizeglobal.com>
2023-11-12 17:47:07 +0400

    fix: minor fix in the example of the system usage

--- usage/util.py
+++ usage/util.py
@@ -4,27 +4,29 @@ from matplotlib import pyplot as plt
 import json
 import numpy as np
 
+
 def convert_bounds(bbox, invert_y=False):
     x1, y1, x2, y2 = bbox
     if invert_y:
         y1, y2 = y2, y1
     return ((y1, x1), (y2, x2))
 
-def visualize(meta_file_name):
-  map = folium.Map()
-  with open(meta_file_name, encoding = 'utf-8') as f:
-    data = json.loads(f.read())
-  for i, im in enumerate(data):
-    bbox = im.get('bbox')
-    folium.GeoJson(
-      shapely.geometry.box(*bbox),
-      style_function=lambda x: dict(fill=True, weight=2, opacity=0.8, color="blue"),
-      name=f"Query_{i}",
-    ).add_to(map)
 
-    image_bounds = convert_bounds(bbox, invert_y=True)
-    img = plt.imread(im.get('filename'), format='jpg')
-    img_ovr = folium.raster_layers.ImageOverlay(
+def visualize(meta_file_name):
+    map = folium.Map()
+    with open(meta_file_name, encoding='utf-8') as f:
+        data = json.loads(f.read())
+    for i, im in enumerate(data):
+        bbox = im.get('bbox')
+        folium.GeoJson(
+            shapely.geometry.box(*bbox),
+            style_function=lambda x: dict(fill=True, weight=2, opacity=0.8, color="blue"),
+            name=f"Query_{i}",
+        ).add_to(map)
+
+        image_bounds = convert_bounds(bbox, invert_y=True)
+        img = plt.imread(im.get('filename'), format='jpg')
+        img_ovr = folium.raster_layers.ImageOverlay(
             name=f"NDVI_{i}",
             image=img,
             bounds=image_bounds,
@@ -32,16 +34,17 @@ def visualize(meta_file_name):
             interactive=True,
             cross_origin=False,
             zindex=1,
-    )
-    img_ovr.add_to(map)
-    map.fit_bounds(bounds=image_bounds)
-      
-  folium.LayerControl().add_to(map)
-  return map
+        )
+        img_ovr.add_to(map)
+        map.fit_bounds(bounds=image_bounds)
 
-def show_legend():
+    folium.LayerControl().add_to(map)
+    return map
+
+
+def show_legend(cmap_name):
     fig, ax = plt.subplots(figsize=(20, 0.5))
-    cmap = plt.cm.RdYlGn
+    cmap = plt.cm.get_cmap(cmap_name)
     values = np.linspace(-1, 1, 256).reshape(1, -1)
     img = ax.imshow(values, cmap=cmap, aspect='auto')
     ax.set_axis_off()
